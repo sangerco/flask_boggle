b576179166e1d14a2606bf8eb695695349ca5c53
sangerco <112724308+sangerco@users.noreply.github.com>
2022-10-28 13:33:38 -0600

    Updated test.py

--- test.py
+++ test.py
@@ -18,21 +18,27 @@ class FlaskTests(TestCase):
                 check that timer is created
             """
             with self.client:
-                resp = self.client.get('/')
-                html = resp.get_data(as_text=True)
-                self.assertEqual(resp.status_code, 200)
+                res = self.client.get('/')
+                html = res.get_data(as_text=True)
+                self.assertEqual(res.status_code, 200)
                 self.assertIn('<table id="board_board">', html)
                 self.assertIn('<b>High Score:</b>', html)
                 self.assertIn('<span class="timer"></span>', html)
 
     def test_for_word_not_on_board(self):
         with app.test_client() as client:
-            resp = client.get('/check-word?word=abracadabra')            
-            html = resp.get_data(as_text=True)
-            self.assertIn('<p class="msg err">abracadabra is not on the board!</p>', html)
+            res = client.get('/check-word?word=abracadabra')
+            html = res.get_data(as_text=True)
+            self.assertIn('<p class="msg err">abracadabra is not on the board!</p>', html)   
+            self.assertEqual(res.json['result'], "not-on-board")
 
-    # def test_for_not_a_word(self):
-    #     with app.test_client() as client:
-    #         resp = client.get('/check-word?word=notaword')
-    #         self.assertEqual(resp.json['result'], "not-word")
+
+
+
+    def test_for_not_a_word(self):
+        with app.test_client() as client:
+            res = client.get('/check-word?word=notaword')
+            html = res.get_data(as_text=True)
+            self.assertIn('<p class="msg err">notaword is not a valid word!</p>', html) 
+            self.assertEqual(res.json['result'], "not-word")
 
